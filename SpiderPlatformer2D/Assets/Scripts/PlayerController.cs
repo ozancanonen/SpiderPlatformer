0fd8867d71a2da96b57d94fc9722665d5566e0ec
ozancanonen <51087802+ozancanonen@users.noreply.github.com>
2020-05-11 23:22:53 +0300

    yeter2

--- SpiderPlatformer2D/Assets/Scripts/PlayerController.cs
+++ SpiderPlatformer2D/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float jumpSpeed = 5f;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 589e89d... Cakisma1
 =======
 >>>>>>> parent of 589e89d... Cakisma1
     //Component Referances
@@ -65,7 +68,10 @@ public class PlayerController : MonoBehaviour
 
             transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f); // return 1 if velocity.x greater than 0 , return -1 if velocity is less than 0;
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 589e89d... Cakisma1
 =======
 >>>>>>> parent of 589e89d... Cakisma1
         }
